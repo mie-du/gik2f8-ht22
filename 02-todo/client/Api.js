026390ad1ea8fc56ff62be407cc7b951c7eba0d2
Mikaela Hedberg <mikaela.hedberg@gmail.com>
2022-12-11 15:33:15 +0100

    Kommentarer för api-klass påbörjade

--- 02-todo/client/Api.js
+++ 02-todo/client/Api.js
@@ -1,15 +1,36 @@
+/* Klass för att hantera API-förfrågningar mot server (lokalt backend skapat i lektion 5) */
 class Api {
+  /* Medlemsvariabel url, för att lagra en grund-url till servern. */
   url = '';
 
+  /* När en instans av klassen skapas skickas url in */
   constructor(url) {
+    /* Medlemsvariabeln url sätts till den sträng som man skickade in när man skapade en instans av klassen (som görs i script.js). Detta upplägg är för att göra denna klass generell. Tanken är att det ska gå att använda vår api-klass till olika HTTP-anrop, inte bara sådana för våran todo-applikation.   */
     this.url = url;
   }
 
-  /* Create = POST */
+  /* Metod för att hatera att skapa resurser. Motsvarar ett anrop med metoden POST. 
+  
+  Create - POST
+  */
   create(data) {
+    /* Konverterar inskickat JavaScriptobjekt, i vårt fall en uppgift, till en sträng så att den kan skickas över HTTP. */
     const JSONData = JSON.stringify(data);
+    /* Log för att se vad som ska skickas och vart det ska skickas */
     console.log(`Sending ${JSONData} to ${this.url}`);
 
+    /* Skapar ett requestobjekt. Requestobjekt finns inbyggda i JavaScript. 
+    Till request-objektets konstruktor skickas
+    1. URL:en dit man vill göra förfrågan. I vårt fall det som man skickade in när klassen skapades och som lagrades i medlemsvariabeln this.url. 
+    2. Ett objekt med konfiguration rörande förfrågan. 
+    
+    Method har satts till "POST". Eftersom vi här ska skapa något så är POST den metod man vill ha. 
+
+    Body har satts till den sträng som skapades utifrån det objekt som skickades in så att innehållet - body - i förfrågan kommer att innehålla en strängrepresentation av det som vi vill skapa. I fallet med vår todo-applikaiton kommer det att vara den uppgift som vi vill spara till servern. 
+
+    Headers är metadata som beskriver olika saker om själva förfrågan. Headers skickas in i form av ett JavaScript-objekt och här sätts egenskapen content-type för att beskriva på vilket sätt data är formaterat så att servern vet hur det ska avkodas när det packas upp på serversidan. 
+      
+    */
     const request = new Request(this.url, {
       method: 'POST',
       body: JSONData,
@@ -18,13 +39,22 @@ class Api {
       }
     });
 
-    return fetch(request)
-      .then((result) => result.json())
-      .then((data) => data)
-      .catch((err) => console.log(err));
+    /* JavaScripts inbyggda funktion fetch är det som används för att göra HTTP-anrop. Fetch tar ett requestobjekt som parameter. Här skickar vi in det requestobjekt som vi skapade direkt ovanför.  */
+
+    /* Fetch är asynkron och vi bearbetar förfrågan och svar i flera olika steg med hjälp av then. Slutligen, när hela "then"-kedjan är färdig, kommer resultatet av det hela att returneras ur denna create-metod. Det är därför hela fetch och alla dess then är omslutna av parenteser och står efter return. Man returnerar alltså hela det uttrycket ut ur metoden.  */
+
+    return (
+      fetch(request)
+        /* När förfrågan skickats kommer först ett resultat i ett oläsbart format. Det tas här emot i en parameter som kallas result, så det avkodas med hjälp av metoden json som finns på result-objektet. result.json() är också asynkrion */
+        .then((result) => result.json())
+        /* Output från result.json() bearbetas genom att det bara tas emot och skickas vidare (data) => data är en förkortning av function(data) {return data}, där data då alltså är resultatet av den asynkrona metoden result.json(). */
+        .then((data) => data)
+        /* Om något i förfrågan eller svaret går fel, fångas det upp här i catch, där information om felet skrivs ut till loggen.  */
+        .catch((err) => console.log(err))
+    );
   }
 
-  /* Read = GET */
+  /* Read - GET */
   getAll() {
     return fetch(this.url)
       .then((result) => result.json())
