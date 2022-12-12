41edbc8ee960d02759a187e9059d22525a2607c3
Mikaela Hedberg <mie@du.se>
2022-12-12 15:31:48 +0100

    coding from lecture

--- 01-searchlist/api/api.js
+++ 01-searchlist/api/api.js
@@ -9,13 +9,48 @@ async function getAll() {
 }
 
 /* Skapa funktion för att spara något (api.js) */
+async function create(objectToCreateAtServer) {
+  /* Ta emot det som ska sparas */
 
-/* Ta emot det som ska sparas */
+  /* objectToCreateAtServer - JavaScript-objekt 
+  {
+    name: "Hej"
+  }
+  */
 
-/* Gör om det till JSON */
+  /* Gör om det till JSON */
+  const JSONData = JSON.stringify(objectToCreateAtServer);
 
-/* Skapa ett requestobjekt */
+  /* Skapa ett requestobjekt */
 
-/* Skicka förfrågan */
+  /* Metoden POST */
+  const request = new Request(url, {
+    method: 'POST',
+    body: JSONData
+  });
 
-/* Returnera förfrågans svar i div apiData. */
+  /* const result = fetch(url, {
+    method: 'POST',
+    body: JSONData
+  }) */
+
+  /* Det som finns i promiset som fetch returneras skickas till callbackfunktion som skickas till then.  */
+  /* const jsonResult = fetch(request).then((response) => response.json()); */
+
+  /* const objResult = jsonResult.then((data) => data); */
+  /* const objResult = jsonResult.then(handleTranslatedResult); */
+
+  /*   function handleTranslatedResult(data) {
+           return data(); 
+        } 
+  */
+
+  /*   const objResult = jsonResult.then(function (data) {
+    return data;
+  });
+ */
+  return [
+    { id: 1, title: 'hej', author: 'hej' },
+    { id: 2, title: 'hej', author: 'hej' }
+  ];
+}
--- 01-searchlist/extra.js
+++ 01-searchlist/extra.js
@@ -1,7 +1,27 @@
 /* Skapa funktion för att spara något (api.js) */
+/* Fungerar inte */
+/* const data = create({ title: 'hej', author: 'hej' }).then((data) => {
+  return data;
+}); */
 
-/* Anropa create-funktion (script.js) */
+const data = create({ title: 'hej', author: 'hej' }).then((data) => {
+  /* Hämta element där data ska skrivas ut */
 
-/* Hämta element där data ska skrivas ut */
+  /* Skriv det man vill göra med data HÄR, eller skicka till annan funktion */
+  const div = document.getElementById('apiData');
 
-/* Skriv ut data från api */
+  /* let html = ''; */
+
+  data.forEach((obj) => {
+    /* html += `<p>${obj.id}</p>`; */
+    div.insertAdjacentHTML('beforeend', `<p>${obj.id}</p>`);
+  });
+
+  /* console.log(html); */
+
+  div.className = 'test2';
+  /* innerHTML ersätter allt */
+  /* div.innerHTML = html; */
+  /* div.insertAdjacentHTML('beforeend', html); */
+  console.log(div);
+});
