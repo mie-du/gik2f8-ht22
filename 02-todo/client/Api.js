a3ac751a6b3ba143be3d7016cd602906ec7c637e
Mikaela Hedberg <mikaela.hedberg@gmail.com>
2022-12-13 15:48:11 +0100

    comments about every step and hints for lab 2

--- 02-todo/client/Api.js
+++ 02-todo/client/Api.js
@@ -1,34 +1,50 @@
-/* Klass för att hantera API-förfrågningar mot server (lokalt backend skapat i lektion 5) */
+/* I denna fil finns en klass för att hantera API-förfrågningar mot server (servern är det backend som skapades i lektion 5).
+
+Om ni vill testa denna kod, kom då ihåg att starta upp servern. Servern, såsom den ser ut i slutet av Lektion 6, finns i denna samma zip-fil. Om ni skulle köra denna kod mot backend såsom det såg ut efter Lektion 5, skulle det inte fungera, eftersom detta är koden såsom den ser ut efter Lektion 6 och några små förändringar gjordes även i servern under Lektion 6. 
+
+Gör då följande
+1. Öppna en terminal
+2. Skriv cd 02-todo/server (utan citattecken) och sedan enter
+3. Skriv "node app.js" (utan citattecken) och enter. 
+
+Om servern startats korrekt syns nu texten "Server running on http://localhost:5000".
+
+*/
+
+/* För att skapa en klass används nyckelordet class följt av klassens namn. Klasser bör ha stor inledande bokstav och döpas enligt det som kallas PascalCase. Inga parenteser används vid skapande av en klass. */
 class Api {
   /* Medlemsvariabel url, för att lagra en grund-url till servern. */
   url = '';
 
-  /* När en instans av klassen skapas skickas url in */
+  /* När en instans av klassen skapas skickas url in som parametern */
   constructor(url) {
-    /* Medlemsvariabeln url sätts till den sträng som man skickade in när man skapade en instans av klassen (som görs i script.js). Detta upplägg är för att göra denna klass generell. Tanken är att det ska gå att använda vår api-klass till olika HTTP-anrop, inte bara sådana för våran todo-applikation.   */
+    /* Medlemsvariabeln url sätts till den sträng som man skickar in när man skapar en instans av klassen (det görs i script.js). Detta upplägg är för att göra denna klass generell. Tanken är att det ska gå att använda vår api-klass till olika HTTP-anrop, inte bara sådana för våran todo-applikation.   */
     this.url = url;
   }
 
-  /* Metod för att hatera att skapa resurser. Motsvarar ett anrop med metoden POST. 
+  /* Metod för att hatera att skapa resurser (Create i CRUD). Motsvarar ett anrop med metoden POST.   
   
-  Create - POST
+  Create = POST
   */
   create(data) {
-    /* Konverterar inskickat JavaScriptobjekt, i vårt fall en uppgift, till en sträng så att den kan skickas över HTTP. */
+    /* Konverterar inskickat JavaScriptobjekt, i detta fall är det en uppgift, till en sträng så att den kan skickas över HTTP. */
     const JSONData = JSON.stringify(data);
-    /* Log för att se vad som ska skickas och vart det ska skickas */
+    /* Utskrift till logg för att se vad som ska skickas och vart det ska skickas */
     console.log(`Sending ${JSONData} to ${this.url}`);
 
-    /* Skapar ett requestobjekt. Requestobjekt finns inbyggda i JavaScript. 
+    /* Nedan skapas ett requestobjekt. Requestobjekt finns inbyggda i JavaScript tack vare JavaScripts fetch API. 
+
     Till request-objektets konstruktor skickas
     1. URL:en dit man vill göra förfrågan. I vårt fall det som man skickade in när klassen skapades och som lagrades i medlemsvariabeln this.url. 
     2. Ett objekt med konfiguration rörande förfrågan. 
+
+    Objektet har följande egenskaper:
     
-    Method har satts till "POST". Eftersom vi här ska skapa något så är POST den metod man vill ha. 
+    method, som har satts till "POST". Eftersom vi här ska skapa något så är POST den metod man vill ha. 
 
-    Body har satts till den sträng som skapades utifrån det objekt som skickades in så att innehållet - body - i förfrågan kommer att innehålla en strängrepresentation av det som vi vill skapa. I fallet med vår todo-applikaiton kommer det att vara den uppgift som vi vill spara till servern. 
+    body, som har satts till den sträng som skapades utifrån det objekt som skickades in så att innehållet - body - i förfrågan kommer att innehålla en strängrepresentation av det som vi vill skapa. I fallet med vår todo-applikaiton kommer det att vara den uppgift som vi vill spara till servern. 
 
-    Headers är metadata som beskriver olika saker om själva förfrågan. Headers skickas in i form av ett JavaScript-objekt och här sätts egenskapen content-type för att beskriva på vilket sätt data är formaterat så att servern vet hur det ska avkodas när det packas upp på serversidan. 
+    headers, som är metadata som beskriver olika saker om själva förfrågan. Headers skickas in i form av ett JavaScript-objekt det också och här sätts egenskapen content-type för att beskriva på vilket sätt data är formaterat så att servern vet hur det ska avkodas när det packas upp på serversidan. 
       
     */
     const request = new Request(this.url, {
@@ -56,20 +72,23 @@ class Api {
 
   /* Read - GET */
   getAll() {
-    /* I detta fetch-anrop behövs inga särskilda inställningar. Fetch kan ta bara url:en som parameter också, istället för att man skapar ett helt request-objekt. */
+    /* I detta fetch-anrop behövs inga särskilda inställningar. Fetch kan ta bara url:en som parameter också, istället för att man skapar ett helt request-objekt och skickar in det. */
     return fetch(this.url)
       .then((result) => result.json())
       .then((data) => data)
       .catch((err) => console.log(err));
   }
 
-  /* Delete = DELETE */
+  /* Delete = DELETE. Här heter dock metoden som hanterar DELETE-förfrågan "remove". delete får inte användas som metod- funktions- eller variabelnamn i JavaScript, då det är ett ord reserverat av JavaScript-språket självt.  */
   remove(id) {
+    /*  Innan ni går vidare med remove, så måste ni se till att server-koden från L5 har en sak från L5 fixad: 
+      I server/app.js ska  
+        res.header('Access-Control-Allow-Method', '*') 
+      ha ändrats till till 
+        res.header('Access-Control-Allow-Methods', '*'); (Method->Methods, alltså)  */
+
     /* Log för att se att rätt uppgift är på väg att tas bort */
     console.log(`Removing task with id ${id}`);
-    /*  Något som inte gjordes i L5, som måste Fixas: */
-
-    /* !!Fixa i server/app.js --> res.header('Access-Control-Allow-Methods', '*');, starta om server!! */
 
     /* Här behövs, precis som vid POST, lite mer inställningar. Fetch behöver dock inte heller här ett requestobjekt. Det går bra att skicka de sakerna som man skulle ha skickat till requestobjektets konstruktor direkt till fetch-funktionen. 
 
@@ -85,4 +104,30 @@ class Api {
       .then((result) => result)
       .catch((err) => console.log(err));
   }
+
+  /***********************Labb 2 ***********************/
+  /* Här skulle det vara lämpligt att skriva en metod likt getAll, create och delete anropas från script.js när någon har markerat en uppgift som färdig. Denna metod bör ansvara för att göra en PUT eller PATCH-förfrågan till vårt backend, precis som create-metoden ansvarar för att göra ett POST-anrop. Metoden här ska alltså motsvara Update = PUT/PATCH. En sådan förfrågan görs med hjälp av fetch(). 
+  
+  Beroende på om ni gör frontend eller backend först i labben behöver ni på något av ställena bestämma er för en av metoderna PUT eller PATCH för denna förfrågan. (Du får välja själv, läs på om vad som verkar mest vettigt för din lösning). Använder du metoden PATCH här behöver i alla fall det vara patch som tas emot i servern också, app.patch(...), och vice versa om du väljer PUT. 
+  */
+
+  /*   
+  För att utföra en förfrågan med hjälp av fetch() behöver servern veta några saker om förfrågan (request). Först och främst behövs en url dit förfrågan ska skickas, sedan behövs också ett objekt med inställningar och detaljer om förfrågan, detta objekt kallas vidare "{options}". Url och {options} kan sättas antingen i ett requestobjekts konstruktor; new Request(url, {options}), såsom det görs i create-metoden. Eller så skulle man kunna ange allt som annars skulle ha skickats till Request-objektets konstruktor inom parenteserna hos fetch() istället; fetch(url, {options})
+  
+  Här finns mer info om fetch-metoden: 
+  https://developer.mozilla.org/en-US/docs/Web/API/fetch.
+  */
+
+  /* Precis som vid create behöver även här {options} innehålla egenskapen body - dvs. de data som ska skickas till server. */
+
+  /* Det finns några sätt att utforma det som ska skickas i förfrågans body. Oavsett vad vi väljer ska det först översättas till JSON. 
+  
+  Alternativ 1: body består av ett helt task-objekt, som också inkluderar förändringen. Exempel: {id: 1,  title: "x", description: "x", dueDate: "x", completed: true/false}
+  Alternativ 2: request-objektets body består bara av förändringarna och vilken uppgift som ska förändras. Exempel: {id: 1, completed: true/false }
+  
+  Om du hittar något annat sätt som funkar för dig, använd för all del det, så länge det uppnår samma sak. :)
+  
+  */
+
+  /***********************Labb 2 ***********************/
 }
